538687496005ff754400bbe11ee6d2129154eae0
Sumesh Ramakrishnan <1035174 @questgedctvm.com>
2021-09-23 13:14:02 +0530

    Get ALL and POST methods added

--- main.go
+++ main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
@@ -12,12 +15,15 @@ import (
 var db *gorm.DB
 var err error
 
-const dsn = "host=localhost user=postgres password=postgres dbname=employee_mgmnt port=5432 sslmode=disable TimeZone=Asia/Kolkata"
+//const dsn = "host=localhost user=postgres password=postgres dbname=employee_mgmnt port=5432 sslmode=disable TimeZone=Asia/Kolkata"
 
 func InitializeMigration() {
-	db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	dbConnectionString := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=5432 sslmode=disable TimeZone=Asia/Kolkata",
+		os.Getenv("DB_HOST"), os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_DATABASE"))
+
+	db, err = gorm.Open(postgres.Open(dbConnectionString), &gorm.Config{})
 	if err != nil {
-		fmt.Print("db errror" + err.Error())
+		fmt.Print(err.Error())
 	}
 	db.AutoMigrate(&User{})
 }
@@ -29,8 +35,16 @@ func initializeRouter() {
 	http.ListenAndServe(":9000", router)
 }
 func main() {
+	loadEnvVariable()
 	InitializeMigration()
 	initializeRouter()
 
 	fmt.Println("Hello, playground")
+}
+
+func loadEnvVariable() {
+	err := godotenv.Load("config.env")
+	if err != nil {
+		log.Fatal("Error loading .env file", err)
+	}
 }
